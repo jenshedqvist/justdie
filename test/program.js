05aa94888d76480519405de7aeb16d0a8bfcb799
Jens Hedqvist <jens.hedqvist@gmail.com>
2015-09-19 00:12:05 +0200

    Better es6 module structure

--- test/program.js
+++ test/program.js
@@ -1,6 +1,6 @@
 import 'babel/polyfill';
 import test from 'tape';
-import justdie from './../lib/justdie';
+import * as lib from './../lib/justdie';
 
 /** Constants */
 const common_dice = [1, 4, 6, 8, 10, 12, 20, 100];
@@ -14,7 +14,7 @@ test('can create any common dice', assert => {
   assert.plan(common_dice.length);
 
   common_dice.forEach(sides => {
-    let die_type = typeof justdie.die(sides);
+    let die_type = typeof lib.die(sides);
     assert.equal(die_type, 'function', 'a ' + sides + ' sided die is a function');
   });
 });
@@ -23,7 +23,7 @@ test('can roll any SINGLE common dice', assert => {
   assert.plan(common_dice.length);
 
   common_dice.forEach(sides => {
-    let die = justdie.die(sides);
+    let die = lib.die(sides);
     assert.equal(typeof die(), 'number', 'a roll of common die ' + sides + ' returns number');
   });
 });
@@ -34,12 +34,12 @@ test('can roll a POOL of the same dice', assert => {
   common_dice.forEach(sides => {
 
     /** Create 3 identical dices */
-    let die1 = justdie.die(sides);
-    let die2 = justdie.die(sides);
-    let die3 = justdie.die(sides);
+    let die1 = lib.die(sides);
+    let die2 = lib.die(sides);
+    let die3 = lib.die(sides);
 
     /** Roll pool */
-    let dice_pool_roll = justdie.roll(die1, die2, die3);
+    let dice_pool_roll = lib.roll(die1, die2, die3);
 
     /** Check that a roll of them returns true */
     assert.equal(typeof dice_pool_roll, 'number', 'a roll with three ' + sides + ' sided dices returns a number');
