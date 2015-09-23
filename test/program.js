27dc7b26b4ea0e93f2948c46aeb9e4a7bbd51778
Jens Hedqvist <jens.hedqvist@gmail.com>
2015-09-23 18:50:10 +0200

    Support for dice modifiers

--- test/program.js
+++ test/program.js
@@ -6,6 +6,13 @@ import * as lib from './../lib/justdie';
 const common_dice = [4, 6, 8, 10, 12, 20, 100];
 
 /**
+ * Checks if target is number
+ * @type {number} item A thing to test
+ * @return {boolean}   True if it is a number, false otherwise
+ */
+const is_number = item => (typeof item === "number" && !isNaN(item));
+
+/**
  * Tests
  * Note: all tests written in SIMPLE code, complex code has no place in tests.
  */
@@ -43,22 +50,23 @@ test('can roll a POOL of the same dice', assert => {
     return lib.roll(die1, die2, die3);
 
   });
-  assert.equal(result.every(roll => typeof roll === 'number'), true, `a roll with three dices of any common sided dice returns a number`);
+  assert.equal(result.every(is_number), true, `a roll with three dices of any common sided dice returns a number`);
 });
 
 
 test('can roll a mixed POOL of dice', assert => {
-  assert.plan(1);
+  assert.plan(4);
 
   /** Roll 4 pools of mixed dices */
   let rolls = [
     lib.roll(lib.die(common_dice[0]), lib.die(common_dice[2]), lib.die(common_dice[3])),
     lib.roll(lib.die(common_dice[6]), lib.die(common_dice[1]), lib.die(common_dice[0])),
-    lib.roll(lib.die(common_dice[4]), lib.die(common_dice[4]), lib.die(common_dice[7])),
+    lib.roll(lib.die(common_dice[4]), lib.die(common_dice[4]), lib.die(common_dice[3])),
     lib.roll(lib.die(common_dice[5]), lib.die(common_dice[6]), lib.die(common_dice[1]))
   ];
-
-  assert.equal(rolls.every(roll => typeof roll === 'number'), true, 'a mixed dice pool roll returns a number');
+  rolls.forEach((roll) => {
+    assert.equal(is_number(roll), true, `a mixed dice pool roll returns a number (${roll})`);
+  });
 });
 
 test('can ROLL any SINGLE common dice VERBOSE', assert => {
@@ -73,5 +81,31 @@ test('can ROLL any SINGLE common dice VERBOSE', assert => {
       (Array.isArray(verbose_roll.minmax) && verbose_roll.minmax.length === 2), true,
       `... "minmax" contains min/max value as array (${verbose_roll.minmax.join(', ')})`);
     assert.equal(typeof verbose_roll.sides, 'number', `... "sides" contains number of sides as number (${verbose_roll.sides})`);
+  });
+});
+
+test('can ROLL a SINGLE die and add an modifier', assert => {
+  assert.plan(3);
+
+  let die6plus2 = lib.die(6, 2)();
+  let die8plus12 = lib.die(8, 12)();
+  let die4minus2 = lib.die(4, -2)();
+  assert.equal(is_number(die6plus2) && (die6plus2 >= 0), true, `a roll d6 +2 returns valid  number (${ die6plus2 })`);
+  assert.equal(is_number(die8plus12) && (die8plus12 >= 0), true, `a roll d8 +12 returns valid number (${ die8plus12 })`);
+  assert.equal(is_number(die4minus2) && (die4minus2 >= 0), true, `a roll d4 -2 returns valid number (${ die4minus2 })`);
+});
+
+test('can ROLL a POOL of dices and add an modifier', assert => {
+  assert.plan(4);
+
+  /** Roll 4 pools of mixed dices */
+  let rolls = [
+    lib.roll(lib.die(common_dice[1], 3), lib.die(common_dice[6], -40), lib.die(common_dice[1], 1)),
+    lib.roll(lib.die(common_dice[6], 23), lib.die(common_dice[4], 0), lib.die(common_dice[0], -0)),
+    lib.roll(lib.die(common_dice[2], -3), lib.die(common_dice[3], 13), lib.die(common_dice[5], 7)),
+    lib.roll(lib.die(common_dice[4], 1), lib.die(common_dice[6], -3), lib.die(common_dice[5], -4))
+  ];
+  rolls.forEach((roll) => {
+    assert.equal(is_number(roll), true, `a mixed dice pool roll with modifier returns a number (${roll})`);
   });
 });
